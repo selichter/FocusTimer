7d755effaa2c5c6352da84213fb1b7cc100bae95
slichter <lichter.sarahe@gmail.com>
2021-05-25 20:57:58 -0500

    first commit

---
+++ Shared/FocusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerApp.swift
+//  Shared
+//
+//  Created by Sarah Lichter on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
