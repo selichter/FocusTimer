b20bb8b5e9b67d23f1baf21e87b199ca3d2521f5
Sarah Lichter <sarahlichter@Sarahs-MacBook-Pro-2.local>
2021-06-12 14:42:59 -0500

    reorg

---
+++ Shared/Timer/TimerStatus.swift
@@ -0,0 +1,12 @@
+//
+//  TimerStatus.swift
+//  FocusTimer
+//
+//  Created by Sarah Lichter on 6/12/21.
+//
+
+import Foundation
+
+enum TimerStatus {
+    case pending, inProgress, completed
+}
---
+++ Shared/Timer/TimerType.swift
@@ -0,0 +1,12 @@
+//
+//  TimerType.swift
+//  FocusTimer
+//
+//  Created by Sarah Lichter on 6/12/21.
+//
+
+import Foundation
+
+enum TimerType {
+    case work, rest
+}
